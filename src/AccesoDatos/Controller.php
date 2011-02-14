3c624ebeca52810e66579c08a6ae72e539156d3e
Roberto Sanchez <roberto.uach@gmail.com>
2011-02-14 11:29:10 -0300

    Agregrando el metodo Update al Controller Generico

--- src/AccesoDatos/Controller.php
+++ src/AccesoDatos/Controller.php
@@ -14,21 +14,40 @@ class Controller
 	//Insertar en Valores en tabla mencionada
 	public function Add($tabla , $parametro)
 	{
-			$_Connection->Connect();
-			$query = "INSERT INTO ". $tabla . " VALUES ". $parametro ;
-			$accion = mysql_query($query); 
-			$_Connection->DisConnect();
-			return $accion;
+		$_Connection->Connect();
+		$query = "INSERT INTO ". $tabla . " VALUES ". $parametro ;
+		$accion = mysql_query($query); 
+		$_Connection->DisConnect();
+		return $accion;
 	}
 
 	//Eliminar Registro de tabla mencionada
 	public function Del($tabla , $registro, $parametro)
 	{
-			$_Connection->Connect();
-			$query = "DELETE FROM " . $tabla . " WHERE " . $registro "=" $parametro ;
-			$accion = mysql_query($query); 
-			$_Connection->DisConnect();
-			return $accion;
+		$_Connection->Connect();
+		$query = "DELETE FROM " . $tabla . " WHERE " . $registro "=" $parametro ;
+		$accion = mysql_query($query); 
+		$_Connection->DisConnect();
+		return $accion;
 	}
+	
+	public function Update ($tabla, $registro, $parametro)
+	{
+		$_Connection->Connect();
+		$query = "UPDATE " . $tabla . " SET " ;
+		$claves = array_keys($parametro);
+		$cant = count($parametro);
+		for($i=1; $i<$cant ; $i++)
+		{
+			$query = $query . $claves[$i] ."=". $parametro[$claves[$i]];
+			$aux = $i + 1;
+			if ($aux != $cant or $aux < $cant)
+				$query = $query . ", ";
+		}
+		$query = $query . " WHERE " . $registro."=". $parametro[$claves[0]];
+		return $query;
+		$_Connection->Disconnect();
+	}	
+	
 }
 ?>
