265d897a51613c0f789e44753e64ec7a72b77d0f
Roberto Sanchez <roberto.uach@gmail.com>
2011-02-16 16:52:32 -0300

    Creado la interfaz para la clase direccion

---
+++ src/SistemaBodegas/Direccion.php
@@ -0,0 +1,19 @@
+<body>
+<p class="tituloHead">Direccion</p>
+<table width="700" align="center" class="filaPar">
+<tr>
+    <td height="31" colspan="6" align="left">
+        <p align="left" class="respDetalleAD">  </p>
+        </td>
+</tr>
+
+<tr>
+<td>Encargado</td>
+<td colspan="5"><input name="encargadoobra" type="text" size="86"/></td>
+</tr>
+<tr>
+<td colspan="6" align="center"><input type="submit" name="Ingresar2" value="Ingresar" />
+ </td>
+</tr>
+</table>
+</body>
