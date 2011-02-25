b8e5977588af245de6104b15e15b0bb0d2d27f92
Alba <alba@Highlander.(none)>
2011-02-25 12:56:57 -0300

    nuevo menu mantencion

--- src/SistemaBodegas/menu.php
+++ src/SistemaBodegas/menu.php
@@ -1,4 +1,3 @@
-
 <meta http-equiv="content-type" content="text/html" />
 <!-- Begin Vista-Buttons.com HEAD SECTION id=vbUL_wp36t-->
 <link href="menu-files/styles_wp36t.css" type="text/css" rel="stylesheet"/>
@@ -12,13 +11,6 @@
 <li><img src="menu-files/smqo36t.png" alt="Ingresar Orden de Compra" /><a href="paso.php?c=1" title="Ingresar Orden de Compra">Ingresar&nbsp;Orden&nbsp;de&nbsp;Compra</a></li>
 <li><a href="paso.php?c=2" title="Ingresar Material">Ingresar&nbsp;Material</a></li>
 <li><a href="paso.php?c=3" title="Ingresar Custodia">Ingresar&nbsp;Custodia</a></li>
-<li><a href="paso.php?c=4" title="A&ntilde;adir Obra">A&ntilde;adir&nbsp;Obra</a></li>
-<li><a href="paso.php?c=v" title="Tipos de Obras">Tipos de Obras</a></li>
-<li><a href="paso.php?c=e" title="Direcciones">Direcciones</a></li>
-<li><a href="paso.php?c=y" title="Departamentos">Departamentos</a></li>
-<li><a href="paso.php?c=w" title="Unidades">Unidades</a></li>
-<li><a href="paso.php?c=z" title="Ciudades">Ciudades</a></li>
-<li><a href="paso.php?c=x" title="Bodegas">Bodegas</a></li>
 <li><a href="paso.php?c=b" title="Ingresar Traspaso">Ingresar&nbsp;Traspaso</a></li></ul></div></td><td style="padding-right:0px" title ="RETIRAR">
 <a onmouseover='xpe("zp36to");xpshow("zp36t",0,this);xpsmover(this);' onmouseout='xpsmout(this);' onmousedown='xpe("zp36tc");'><img id="xpi_zp36t" src="menu-files/btzp36t_0.png" name="vbzp36t" width="107" height="37" border="0" alt="RETIRAR" /></a><div>
 <ul id="vbUL_zp36t" class="vbULwp36t">
@@ -26,12 +18,25 @@
 <li><a href="paso.php?c=6" title="Retirar Custodia">Retirar&nbsp;Custodia</a></li>
 <li><a href="paso.php?c=7" title="Eliminar Obra">Eliminar&nbsp;Obra</a></li>
 <li><a href="paso.php?c=c" title="Eliminar Obra">Salida&nbsp;Traspaso</a>
-<li><a href="paso.php?c=d" title="Eliminar Obra">Salida&nbsp;Merma</a></li></ul></div></td><td style="padding-right:0px" title ="USUARIOS">
+<li><a href="paso.php?c=d" title="Eliminar Obra">Salida&nbsp;Merma</a></li></ul></div></td>
+
+<td style="padding-right:0px" title ="MANTENCION">
+<a onmouseover='xpe("wp40to");xpshow("wp40t",0,this);xpsmover(this);' onmouseout='xpsmout(this);' onmousedown='xpe("wp40tc");'><img id="xpi_wp40t" src="menu-files/btwp40t_0.png" name="vbwp40t" width="160" height="37" border="0" alt="MANTENCION" /></a><div>
+<ul id="vbUL_wp40t" class="vbULwp36t">
+<li><a href="paso.php?c=4" title="A&ntilde;adir Obra">A&ntilde;adir&nbsp;Obra</a></li>
+<li><a href="paso.php?c=e" title="Direcciones">Direcciones</a></li>
+<li><a href="paso.php?c=y" title="Departamentos">Departamentos</a></li>
+<li><a href="paso.php?c=w" title="Unidades">Unidades</a></li>
+<li><a href="paso.php?c=z" title="Ciudades">Ciudades</a></li>
+<li><a href="paso.php?c=x" title="Bodegas">Bodegas</a></li></ul></div></td>
+
+<td style="padding-right:0px" title ="USUARIOS">
 <a onmouseover='xpe("up36to");xpshow("up36t",0,this);xpsmover(this);' onmouseout='xpsmout(this);' onmousedown='xpe("up36tc");'><img id="xpi_up36t" src="menu-files/btup36t_0.png" name="vbup36t" width="121" height="37" border="0" alt="USUARIOS" /></a><div>
 <ul id="vbUL_up36t" class="vbULwp36t">
 <li><img src="menu-files/smdo36t.png" alt="Agregar Usuario" /><a href="paso.php?c=8" title="Agregar Usuario">Agregar&nbsp;Usuario</a></li>
 <li><a href="paso.php?c=9" title="Editar y Eliminar Usuario">Editar&nbsp;y&nbsp;Eliminar&nbsp;Usuario</a></li></ul></div></td><td style="padding-right:0px" title ="SALIR">
 <a href="index.php" onmouseover='xpe("vp36to");' onmouseout='xpe("vp36tn");' onmousedown='xpe("vp36tc");'><img id="xpi_vp36t" src="menu-files/btvp36t_0.png" name="vbvp36t" width="100" height="37" border="0" alt="SALIR" /></a></td></tr></table><script type="text/javascript"> var vbImgPath="menu-files/"</script>
 <script type="text/javascript" src="menu-files/scwp36t.js"></script>
+
 
 
